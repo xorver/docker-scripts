f125e64b0ac136eee6d94052e6ff2feef8107549
Tomasz Lichon <xorver@gmail.com>
2016-03-22 18:36:20 +0100

    Update env_description.json, fix wrong user.

--- mount_client.sh
+++ mount_client.sh
@@ -4,11 +4,11 @@ set -v -e
 
 # input
 if [ $1 = "2" ] ; then
-	USERNAME=user1
+	USERNAME=u1
 	WORKER_DOMAIN=worker1.p2
 	CLIENT_DOMAIN=client_host_2
 else
-        USERNAME=user1
+        USERNAME=u1
         WORKER_DOMAIN=worker1.p1
         CLIENT_DOMAIN=client_host_1
 fi
@@ -30,10 +30,6 @@ docker exec -it -u ${USERNAME} ${CLIENT_HASH} \
 	mkdir -p /home/${USERNAME}/.local/share/oneclient/
 docker exec -it -u ${USERNAME} ${CLIENT_HASH} bash -c \
 	"echo ${TOKEN} > /home/${USERNAME}/.local/share/oneclient/token"
-#docker exec -it -u ${USERNAME} ${CLIENT_HASH} bash -c \
-#	"echo \"export HOME=/home/${USERNAME}\" >> /home/${USERNAME}/.bashrc"
-#docker exec -it -u ${USERNAME} ${CLIENT_HASH} bash -c \
-#	"echo \"export USER=${USERNAME}\" >> /home/${USERNAME}/.bashrc"
 
 docker exec -it -u ${USERNAME} ${CLIENT_HASH} bash -c \
     "USER=${USERNAME} HOME=/home/${USERNAME} PROVIDER_HOSTNAME=${WORKER_IP} /home/${USERNAME}/oneclient -d --no_check_certificate --auth token /home/${USERNAME}/onedata"
