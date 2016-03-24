add35cac16b58b25bf89598636330b4ea9abeb56
Tomasz Lichon <xorver@gmail.com>
2016-03-24 10:18:23 +0100

    Add utility script fo copying releases into oneata directory.

---
+++ copy_releases.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+rm -rf ~/onedata/op_worker/rel/op_worker                       && cp -R ~/op-worker/rel/op_worker                       ~/onedata/op_worker/rel/op_worker
+rm -rf ~/onedata/op_worker/cluster_manager/rel/cluster_manager && cp -R ~/op-worker/cluster_manager/rel/cluster_manager ~/onedata/cluster_manager/rel/cluster_manager
+rm -rf ~/onedata/op_worker/appmock/rel/appmock                 && cp -R ~/op-worker/appmock/rel/appmock                 ~/onedata/appmock/rel/appmock
+rm -rf ~/onedata/oz_worker/rel/oz_worker                       && cp -R ~/globalregistry/rel/oz_worker                  ~/onedata/oz_worker/rel/oz_worker
+rm -rf ~/onedata/oneclient/rebug                               && cp -R ~/oneclient/debug                               ~/onedata/oneclient/debug
