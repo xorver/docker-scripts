62a0f99ddfffba800fa106385c85317d3c98f489
Tomasz Lichon <xorver@gmail.com>
2016-03-24 10:17:53 +0100

    Update env_up script.

--- env_up.sh
+++ env_up.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-~/op-worker/bamboos/docker/env_up.py -bw ~/op-worker -bcm ~/op-worker/cluster_manager/ -boz ~/globalregistry -bc ~/oneclient -l ~/op-worker/logs ~/docker-scripts/env_description.json
+~/onedata/bamboos/docker/env_up.py -bw ~/op-worker -bcm ~/op-worker/cluster_manager/ -boz ~/globalregistry -bc ~/oneclient -l ~/op-worker/logs ~/docker-scripts/env_description.json
