c835204311a4209695ba0adc57f647205152dab5
Tomasz Lichon <xorver@gmail.com>
2016-03-29 11:36:39 +0200

    Update copy_releases scripts.

--- copy_releases.sh
+++ copy_releases.sh
@@ -4,4 +4,4 @@ rm -rf ~/onedata/op_worker/rel/op_worker                       && cp -R ~/op-wor
 rm -rf ~/onedata/op_worker/cluster_manager/rel/cluster_manager && cp -R ~/op-worker/cluster_manager/rel/cluster_manager ~/onedata/cluster_manager/rel/cluster_manager
 rm -rf ~/onedata/op_worker/appmock/rel/appmock                 && cp -R ~/op-worker/appmock/rel/appmock                 ~/onedata/appmock/rel/appmock
 rm -rf ~/onedata/oz_worker/rel/oz_worker                       && cp -R ~/globalregistry/rel/oz_worker                  ~/onedata/oz_worker/rel/oz_worker
-rm -rf ~/onedata/oneclient/rebug                               && cp -R ~/oneclient/debug                               ~/onedata/oneclient/debug
+rm -rf ~/onedata/oneclient/debug                               && cp -R ~/oneclient/debug                               ~/onedata/oneclient/debug
