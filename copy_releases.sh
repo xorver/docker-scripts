9b10e23cda6edd9d57b97c1786a723010ca8542c
Tomasz Lichon <xorver@gmail.com>
2016-04-07 15:59:02 +0200

    Update copy_releases.

--- copy_releases.sh
+++ copy_releases.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-rm -rf ~/onedata/op_worker/rel/op_worker                       && cp -R ~/op-worker/rel/op_worker                       ~/onedata/op_worker/rel/op_worker
-rm -rf ~/onedata/op_worker/cluster_manager/rel/cluster_manager && cp -R ~/op-worker/cluster_manager/rel/cluster_manager ~/onedata/cluster_manager/rel/cluster_manager
-rm -rf ~/onedata/op_worker/appmock/rel/appmock                 && cp -R ~/op-worker/appmock/rel/appmock                 ~/onedata/appmock/rel/appmock
-rm -rf ~/onedata/oz_worker/rel/oz_worker                       && cp -R ~/globalregistry/rel/oz_worker                  ~/onedata/oz_worker/rel/oz_worker
-rm -rf ~/onedata/oneclient/debug                               && cp -R ~/oneclient/debug                               ~/onedata/oneclient/debug
+rm -rf ~/one_projects/onedata/op_worker/rel/op_worker                       && cp -R ~/one_projects/op-worker/rel/op_worker                       ~/one_projects/onedata/op_worker/rel/op_worker
+rm -rf ~/one_projects/onedata/op_worker/cluster_manager/rel/cluster_manager && cp -R ~/one_projects/op-worker/cluster_manager/rel/cluster_manager ~/one_projects/onedata/cluster_manager/rel/cluster_manager
+rm -rf ~/one_projects/onedata/op_worker/appmock/rel/appmock                 && cp -R ~/one_projects/op-worker/appmock/rel/appmock                 ~/one_projects/onedata/appmock/rel/appmock
+rm -rf ~/one_projects/onedata/oz_worker/rel/oz_worker                       && cp -R ~/one_projects/globalregistry/rel/oz_worker                  ~/one_projects/onedata/oz_worker/rel/oz_worker
+rm -rf ~/one_projects/onedata/oneclient/debug                               && cp -R ~/one_projects/oneclient/debug                               ~/one_projects/onedata/oneclient/debug
