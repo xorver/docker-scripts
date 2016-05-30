60108603c03acdb1a2cb1e7ac14d1510a6b23e5b
Tomasz Lichon <xorver@gmail.com>
2016-05-30 10:49:44 +0200

    Change path from /onedata to /onedata-2 in copy_releases_2 script.

--- copy_releases_2.sh
+++ copy_releases_2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-rm -rf ~/one_projects/onedata/op_worker/rel/op_worker                       && cp -R ~/one_projects/op-worker/rel/op_worker                       ~/one_projects/onedata/op_worker/rel/op_worker
-rm -rf ~/one_projects/onedata/op_worker/cluster_manager/rel/cluster_manager && cp -R ~/one_projects/op-worker/cluster_manager/rel/cluster_manager ~/one_projects/onedata/cluster_manager/rel/cluster_manager
-rm -rf ~/one_projects/onedata/op_worker/appmock/rel/appmock                 && cp -R ~/one_projects/op-worker/appmock/rel/appmock                 ~/one_projects/onedata/appmock/rel/appmock
-rm -rf ~/one_projects/onedata/oz_worker/rel/oz_worker                       && cp -R ~/one_projects/oz-worker/rel/oz_worker                       ~/one_projects/onedata/oz_worker/rel/oz_worker
-rm -rf ~/one_projects/onedata/oneclient/debug                               && cp -R ~/one_projects/oneclient/debug                               ~/one_projects/onedata/oneclient/debug
+rm -rf ~/one_projects/onedata-2/op_worker/rel/op_worker                       && cp -R ~/one_projects/op-worker/rel/op_worker                       ~/one_projects/onedata-2/op_worker/rel/op_worker
+rm -rf ~/one_projects/onedata-2/op_worker/cluster_manager/rel/cluster_manager && cp -R ~/one_projects/op-worker/cluster_manager/rel/cluster_manager ~/one_projects/onedata-2/cluster_manager/rel/cluster_manager
+rm -rf ~/one_projects/onedata-2/op_worker/appmock/rel/appmock                 && cp -R ~/one_projects/op-worker/appmock/rel/appmock                 ~/one_projects/onedata-2/appmock/rel/appmock
+rm -rf ~/one_projects/onedata-2/oz_worker/rel/oz_worker                       && cp -R ~/one_projects/oz-worker/rel/oz_worker                       ~/one_projects/onedata-2/oz_worker/rel/oz_worker
+rm -rf ~/one_projects/onedata-2/oneclient/debug                               && cp -R ~/one_projects/oneclient/debug                               ~/one_projects/onedata-2/oneclient/debug
